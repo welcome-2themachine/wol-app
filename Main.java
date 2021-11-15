0c3ad49956b605ea688893ce0ee5b5361a1c786f
welcome-2themachine <christopher.prevost94@gmail.com>
2021-11-14 20:29:03 -1000

    Created the skeleton of a hello world

--- Main.java
+++ Main.java
@@ -4,3 +4,10 @@
 *   Created Date: Sunday 14 November, 2021 @ 2014 HST
 *   Author: welcome-2themachineS
 */
+
+class Wol_App{
+
+    public static void main(String[] args){
+        System.out.println("Hello World");
+    }
+}
